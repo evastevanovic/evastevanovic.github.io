3fcd0d0fe1d4bccae4ab23977f1e7382602a0b1d
evastevanovic <evasd@comcast.net>
2026-08-13 02:57:03 -0400

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4173
-  }
+  base: '/portfolio-site/', // Replace with your repository name (e.g., '/portfolio/')
 })
